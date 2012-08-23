70af2c694749ae2451f561fbc91a632b4db052ce
Bruce Lv <bruce.lv@nokia.com>
2012-08-23 09:48:48 +0800

    fix bugs in sort

--- script/sort.py
+++ script/sort.py
@@ -1,17 +1,16 @@
 def shell_sort(ary, step):
-    print(ary)
     for i in range(step, len(ary)):
-        for j in range(i, 0, -step):
-            if ary[j] > ary[j-step]:
-                break
-            else:
-                ary[j], ary[j-step] = ary[j-step], ary[j]
-    print(ary)
+        pos = i - step
+        while pos >= 0:
+            if ary[pos + step] < ary[pos]:
+                ary[pos + step], ary[pos] = ary[pos], ary[pos + step]
+            pos -= step
+    print(" ".join(ary))
 
 def selection_sort(ary, times=None):
     for i in range(0, len(ary)):
         
-        if times and times == i + 1:
+        if times and times == i:
             print(times)
             print(" ".join(ary))
         
@@ -23,33 +22,28 @@ def selection_sort(ary, times=None):
         ary[i],ary[min_pos] = ary[min_pos], ary[i]
         
 
-        
-    print(ary)
-
 def insertion_sort(ary, times=None):
-    print(ary)
+    exchanges = 0
     for i in range(1, len(ary)):
-        if times and times == i:
-            print(times)
-            print(" ".join(ary))
-        
         if ary[i] > ary[i-1]:
             continue
         
         for j in range(i, 0, -1):
             if ary[j] < ary[j-1]:
                 ary[j], ary[j-1] = ary[j-1], ary[j]
+                exchanges += 1
+                if times and times == exchanges:
+                    print(exchanges)
+                    print(" ".join(ary))
             else:
-                break;       
-    print(ary)
+                break
 
 if __name__ == "__main__":
-    
-    selection_sort("L O F E K U R S Q H".split(), 4)
-    insertion_sort("L O F E K U R S Q H".split(), 4)
-    ary = "L O F E K U R S Q H".split()
+    selection_sort("J Z H P N M A S V U".split(), 4)
+    insertion_sort("G I O Q U S Y J D E".split(), 6)
+    ary = "X U B Z R G W V T A".split()
     shell_sort(ary, 4)
-    shell_sort(ary, 1)
+    #shell_sort(ary, 1)
     
 #    for i in range(4, 10):
 #        print(i)
