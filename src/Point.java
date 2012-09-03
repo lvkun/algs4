075bd0fece982986caef043bbd68d4575311f00a
Bruce Lv <bruce.lv@nokia.com>
2012-09-03 17:03:43 +0800

    Point

---
+++ src/Point.java
@@ -0,0 +1,71 @@
+/*************************************************************************
+ * Name:
+ * Email:
+ *
+ * Compilation:  javac Point.java
+ * Execution:
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ *
+ *************************************************************************/
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER = new BySlope(); 
+
+    private final int x;                              // x coordinate
+    private final int y;                              // y coordinate
+
+    private static class BySlope implements Comparator<Point> {
+        public int compare(Point p1, Point p2) {
+            return 0;
+        }
+    }
+    
+    // create the point (x, y)
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        /* YOUR CODE HERE */
+        return 0;
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+        return 0;
+    }
+
+    // return string representation of this point
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+}
