283f66c63f4d6d0881e9b1ff37c9dcf218f2c56c
Bruce Lv <bruce.lv@nokia.com>
2012-09-04 14:07:03 +0800

    brute

---
+++ src/Brute.java
@@ -0,0 +1,67 @@
+
+
+/**
+ * @author brlv
+ *
+ */
+public class Brute {
+    
+    /**
+     * @param pointInfo
+     * @return
+     */
+    private static Point createPoint(String pointInfo) {
+        String[] point = pointInfo.trim().split("\\s+");
+        if (point.length != 2) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        int x = Integer.parseInt(point[0]);
+        int y = Integer.parseInt(point[1]);
+        
+        return new Point(x, y);
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        
+        In in = new In(args[0]);
+        int pointNumber = Integer.parseInt(in.readLine());
+        
+        Point[] points = new Point[pointNumber];
+        int index = 0;
+        
+        if (pointNumber > 0) {
+            while (!in.isEmpty() && index < pointNumber) {
+                String line = in.readLine();
+                points[index++] = createPoint(line);
+            }
+        }
+        
+        
+        Quick.sort(points);
+        
+        for (int i = 0; i < pointNumber - 3; i++) {
+            Point p1 = points[i];
+            for (int j = i + 1; j < pointNumber - 2; j++) {
+                Point p2 = points[j];
+                for (int k = j + 1; k < pointNumber - 1; k++) {
+                    Point p3 = points[k];
+                    for (int l = k + 1; l < pointNumber; l++) {
+                        Point p4 = points[l];
+                        if ((p1.slopeTo(p2) == p1.slopeTo(p3))
+                                && (p1.slopeTo(p2) == p1.slopeTo(p4))) {
+                            StdOut.println(p1 + " -> " + p2 
+                                    + " -> " + p3 + " -> " + p4);
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
