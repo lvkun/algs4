4143d3e86b8445c5613e75072bbd44873ba92903
Bruce Lv <bruce.lv@nokia.com>
2013-04-03 16:12:32 +0800

    api - wordnet

---
+++ src/Outcast.java
@@ -0,0 +1,23 @@
+
+public class Outcast {
+    /* 
+     * constructor takes a WordNet object
+     */
+    public Outcast(WordNet wordnet) {
+        
+    }
+
+    /* 
+     * given an array of WordNet nouns, return an outcast
+     */
+    public String outcast(String[] nouns) {
+        return null;
+    }
+
+    /* 
+     * for unit testing of this class (such as the one below)
+     */
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/WordNet.java
@@ -0,0 +1,45 @@
+
+public class WordNet {
+    /* 
+     * constructor takes the name of the two input files
+     */
+    public WordNet(String synsets, String hypernyms){
+        
+    }
+
+    /*
+     * returns all WordNet nouns
+     */
+    public Iterable<String> nouns() {
+        return null;
+    }
+
+    /*
+     * is the word a WordNet noun?
+     */
+    public boolean isNoun(String word) {
+        return true;
+    }
+
+    /*
+     * distance between nounA and nounB (defined below)
+     */
+    public int distance(String nounA, String nounB) {
+        return 0;
+    }
+
+    /*
+     * a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+     * in a shortest ancestral path (defined below)
+     */
+    public String sap(String nounA, String nounB) {
+        return null;
+    }
+
+    /*
+     * for unit testing of this class
+     */
+    public static void main(String[] args) {
+        
+    }
+}
